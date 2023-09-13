daf4998de44fb3cfb16dd095a8b4105c726da04a
EC2 Default User <ec2-user@ip-172-31-26-18.ec2.internal>
2023-09-13 08:39:35 +0000

    resolve

--- app/api/api.py
+++ app/api/api.py
@@ -13,34 +13,18 @@ from PIL import Image
 def prod_overview(url): #amazon s3 url. The location of the image
   device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
-  processor = Blip2Processor.from_pretrained("Salesforce/blip2-flan-t5-xxl")
-  B_model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-flan-t5-xl",torch_dtype=torch.float16, device_map="auto")
+  processor = Blip2Processor.from_pretrained("Salesforce/blip2-flan-t5-xxl",cache_dir="/tmp/")
+  B_model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-flan-t5-xl",torch_dtype=torch.float32, device_map="auto",cache_dir="/tmp/")
 
-  tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token='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')
-  model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", device_map='auto',torch_dtype=torch.float16, use_auth_token="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")
+  tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token='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',cache_dir="/tmp/")
+  model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", device_map='auto',torch_dtype=torch.float32, use_auth_token="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",cache_dir="/tmp/")
 
   B_INST, E_INST = "[INST]", "[/INST]"
   B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
   DEFAULT_SYSTEM_PROMPT = """\
   The structure of the output should look like this only, for example:
 
-  overview: 'Here's a product description for the Apple iPhone 11
-  Pro 256GB Space Grey: The Apple iPhone 11 Pro is the latest flagship device from Apple, featuring a
-  powerful A13 Bionic chip, a stunning 6.1-inch Super Retina HD display, and an impressive quad-camera
-  system. With a sleek and durable design, this phone is sure to turn heads. The A13 Bionic chip
-  provides lightning-fast performance and efficient battery life, allowing you to multitask with ease
-  and enjoy your favorite apps and games without worrying about running out of juice. The quad-camera
-  system includes a wide-angle lens, a telephoto lens, and a macro lens, giving you more creative
-  options when capturing photos and videos. The iPhone 11 Pro also features a high-quality audio
-  experience, with improved speakers and a new spatial audio feature that immerses you in your
-  favorite music, movies, and games. With Apple's advanced Face ID technology, you can unlock your
-  phone with just a glance, and the phone's long-lasting battery ensures that you can use it all day
-  without needing to recharge. In terms of design, the iPhone 11 Pro features a sleek and durable
-  stainless steel and glass construction, available in three gorgeous colors: Space Grey, Gold, and
-  Silver. The phone's IP68 rating means it can withstand being submerged in water up to 4 meters for
-  up to 30 minutes, making it perfect for those who love to take their phone with them wherever they
-  go.I hope this product description helps! Let me know if you have
-  any other questions.', 'estimated_price': '$324'
+  overview: 'some text', 'estimated_price': '$3'
 
   this is just an example for the products. Give me output in these way. Do not generate any additional dialogues
 
@@ -69,8 +53,8 @@ def prod_overview(url): #amazon s3 url. The location of the image
 
   def generate(text):
       prompt = get_prompt(text)
-      with torch.autocast('cuda', dtype=torch.bfloat16):
-          inputs = tokenizer(prompt, return_tensors="pt").to('cuda')
+      with torch.autocast('cpu', dtype=torch.bfloat16):
+          inputs = tokenizer(prompt, return_tensors="pt").to("cpu")
           outputs = model.generate(**inputs,
                                   max_new_tokens=512,
                                   eos_token_id=tokenizer.eos_token_id,
@@ -90,10 +74,13 @@ def prod_overview(url): #amazon s3 url. The location of the image
 
   img_url = url
   raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
-  question = "what is in the image?"
-  inputs = processor(raw_image, question, return_tensors="pt").to("cuda")
+  question = "in 5 words tell me , what is in the image?"
+  inputs = processor(raw_image, question, return_tensors="pt").to("cpu")
   out = B_model.generate(**inputs)
   prompt = processor.decode(out[0], skip_special_tokens=True)
+  print("======")
+  print(prompt)
+  print("======")
   generated_text = generate(prompt)
 
   return parse_text(generated_text)
--- app/main.py
+++ app/main.py
@@ -46,7 +46,10 @@ def upload_file(file: UploadFile = File(...)):
     s3 = session.resource('s3')
     s3.meta.client.upload_file(file_location, 'neuralgobucket', f"images/{file.filename}")
     s3_url=f"https://neuralgobucket.s3.amazonaws.com/images/{file.filename}"
-
-    text=api.prod_overview(s3_url)
-    
-    return {"response":text}
+    try:
+        text=api.prod_overview(s3_url)
+        est_price=api.est_price(text)
+    except Exception as e:
+        print("error in generating text or estimated price regex")
+        return {"response":"","estimated_price":""}
+    return {"response":text,"estimated_price":est_price}
